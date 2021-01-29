ed24c5ec3f17db7c7cfc3df2608641c8fa6b513a
Narcis Gomez Quintana <narcisgomezquintana@gmail.com>
2021-01-29 13:29:56 +0100

    basic game without hooks

--- src/game.js
+++ src/game.js
@@ -1,76 +1,103 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Board from './board';
 
-function Game () {
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
 
-  const [history, setHistory] = useState([{squares: Array(9).fill(null),}]);
-  const [stepNumber, setStepNumber] = useState(0);
-  const [xIsNext, setNext] = useState(true);
-
-  const handleClick = (i) => {
-    const his = history.slice(0, stepNumber + 1);
-    const current = his[history.length - 1];
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) return;
-    squares[i] = xIsNext ? 'X' : 'O';
-      setHistory (his.concat([{squares: squares,}]));
-      setStepNumber(his.length);
-      setNext(!xIsNext)
-  }
-
-  const jumpTo = (step) => {
-    setStepNumber(step);
-    setNext((step % 2) === 0);
-  }
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
-    return null;
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
-    const current = history[stepNumber];
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ? 'Go to move #' + move : 'Go to game start';
-
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => jumpTo(move)}>{desc}</button>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
-      )
-    })
+      );
+    });
 
     let status;
-    if (winner) status = 'winner: ' + winner;
-    else status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
 
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares = {current.squares} onClick={(i) => handleClick(i)}/>
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{ status }</div>
-          <ol>{ moves }</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
+  }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 export default Game;
